f15f0d47b8fb5ac49ec2996ec5384a95a3d22bc7
Shimi Halperin <sqh@me.com>
2024-08-27 14:31:22 -0400

    ...

--- counter.js
+++ counter.js
@@ -1,3 +1,3 @@
 
-let currentIndex = 3;  // Start with the last file (e.g., 00000003.md)
+let currentIndex = 4;  // Start with the last file (e.g., 00000003.md)
 const totalFiles = currentIndex;  // Adjust this based on the number of markdown files you have
