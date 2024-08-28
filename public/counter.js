4a6230efcc776e41f842a9307a9ac0401fba49aa
Shimi Halperin <sqh@me.com>
2024-08-28 05:58:07 -0400

    ...

--- public/counter.js
+++ public/counter.js
@@ -1,3 +1,3 @@
 
-let currentIndex = 1;  // Start with the last file (e.g., 00000003.md)
+let currentIndex = 2;  // Start with the last file (e.g., 00000003.md)
 const totalFiles = currentIndex;  // Adjust this based on the number of markdown files you have
--- public/engine1.js
+++ public/engine1.js
@@ -1,8 +1,12 @@
-const baseURL = 'https://shalperin.github.io/microblog/microblogs/';
+const baseURL = 'https://microblog-e0839.web.app/microblogs/';
 
     const folderPath = '/microblogs/';
 
-    function loadMarkdown(index) {
+    function loadPost(index) {
+        console.log(`trying to load  post ${index}`)
+        if (index <0 || index > totalFiles) {
+            index = totalFiles
+        }
         /* I'm going to guess that
         ```
         python3 -m http.server
@@ -14,15 +18,16 @@ const baseURL = 'https://shalperin.github.io/microblog/microblogs/';
         const filePath = `../death.md` // is correctly a network error.
 
         */
-
-        console.log(prod)
+        
+        // console.log(`prod = prod`)
         var filePath = ""
         if (prod) {
             filePath = `${baseURL}${String(index).padStart(8, '0')}.md`;
         } else {
             filePath = `${folderPath}${String(index).padStart(8, '0')}.md`;
         }
-        console.log(filePath)
+        //console.log(filePath)
+        
         fetch(filePath)
             .then(response => {
                 if (!response.ok) {
@@ -32,13 +37,40 @@ const baseURL = 'https://shalperin.github.io/microblog/microblogs/';
             })
             .then(markdown => {
                 document.getElementById('markdown-content').innerHTML = marked.parse(markdown);
-                updateButtons();
+                updateButtons()
             })
             .catch(error => {
                 console.error('Error fetching markdown file:', error);
             });
     }
 
+    function pushHash(index) {
+        let state = `/post/${index}`
+        console.log(`pushing or repushing hash ${state}`)
+        window.location.hash = state;
+    }
+    
+    function hashChange() {
+        let postId = getHashState();
+        console.log(`hash changed - postId is ${postId} in hashchange`)
+        currentIndex = postId
+        loadPost(postId);
+    }
+    
+    function getHashState() {
+        // console.log("checking hash state")
+        const hash = window.location.hash
+        if (hash.startsWith('#/post/')) {
+            // console.log("have hash state")
+            const postId = hash.split('/')[2];
+            // console.log(`postId is ${postId}`)
+            return postId;
+        } else {
+            // console.log("no hash state")
+        }
+    }
+
+    
     function updateButtons() {
         document.getElementById('next-button').disabled = currentIndex <= 1;
         document.getElementById('back-button').disabled = currentIndex >= totalFiles;
@@ -47,16 +79,29 @@ const baseURL = 'https://shalperin.github.io/microblog/microblogs/';
     document.getElementById('next-button').addEventListener('click', () => {
         if (currentIndex > 1) {
             currentIndex--;
-            loadMarkdown(currentIndex);
+            pushHash(currentIndex)
         }
     });
 
     document.getElementById('back-button').addEventListener('click', () => {
         if (currentIndex < totalFiles) {
             currentIndex++;
-            loadMarkdown(currentIndex);
+            pushHash(currentIndex)
         }
     });
 
+    console.log("adding hash change listener")
+    window.addEventListener('hashchange', hashChange)
+
     // Load the last markdown file on page load
-    loadMarkdown(currentIndex);
+    let index = getHashState()
+    if (index === undefined) {
+        console.log("It looks like we tried to load the blog root.")
+        pushHash(currentIndex)
+    } else {
+        console.log(`it looks like we tried to load a specfic page index is ${index}`)
+        currentIndex = index
+        loadPost(currentIndex)
+    }
+    
+//    loadPost(currentIndex);
